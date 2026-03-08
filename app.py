be8334f536b11c1c6f506155cba9a2e808ed9264
Kyrino <danielbaena71@gmail.com>
2026-03-08 18:54:33 -0500

    add team Brighton, Valencia,Galatasaray,Inter Miami,América,Lyon

--- app.py
+++ app.py
@@ -149,6 +149,7 @@ else:
         "Barcelona", "PSG", "Inglaterra", "Portugal", "España", "Francia", "Holanda", "Italia", 
         "Alemania", "Brasil", "Argentina", "Colombia", "Newcastle", "Atalanta", "Betis", "Benfica",
         "Flamengo", "Bélgica","Suiza","Dinamarca","Polonia","Serbia","Marruecos","Uruguay", "Atletico Nacional","A tu Eleccion"
+        "Brighton", "Valencia","Galatasaray","Inter Miami","América","Lyon"
     ]
 
     st.divider()
